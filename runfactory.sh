482dd4b6f28aa95b4afe288ee8916bb255cd7e6d
The Gentlehacker <76982813+TheGentlehacker@users.noreply.github.com>
2025-04-27 23:14:19 +1000

    Update runfactory.sh

--- runfactory.sh
+++ runfactory.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-podman run --detach --name=factory-game --hostname factory.gentlehacker.io --restart unless-stopped --volume ../gamedata:/config --env MAXPLAYERS=4 --env PGID=1000 --env PUID=1000 --env STEAMBETA=false --memory-reservation=4G --memory 8G --publish 7777:7777/tcp --publish 7777:7777/udp --publish 8888:8888/tcp wolveix/satisfactory-server:latest"
+podman run --detach --name=factory-game --hostname factory.gentlehacker.io --restart unless-stopped --volume /mnt/container_data/factory/gamedata:/config --env MAXPLAYERS=4 --env PGID=1000 --env PUID=1000 --env STEAMBETA=false --memory-reservation=4G --memory 8G --publish 7777:7777/tcp --publish 7777:7777/udp --publish 8888:8888/tcp wolveix/satisfactory-server:latest"
